7d0fa816df89e8cb34de18423578d49460df0d6f
gloryakinyi <akinyiglory2@gmail.com>
2025-02-02 07:56:24 +0300

    initial commit

--- app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
@@ -5,19 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,27 +67,68 @@ fun Home() {
 
     var name by remember { mutableStateOf("") }
 
-
-    val myButtonText = remember{
+    val myButtonText = remember {
         mutableStateOf("Do your magic")
     }
 
+    val buttonStatus = remember {
+        mutableStateOf(true)
+    }
+
+    val myImage = remember {
+        mutableStateOf(R.drawable.first)
+    }
+
+    //Dynamic text with checkbox
+    var myText = remember {
+        mutableStateOf("What is your gender?")
+    }
+
+    var firstCheckbox = remember {
+        mutableStateOf(false)
+    }
+
+    var secondCheckbox = remember {
+        mutableStateOf(false)
+    }
+
+    //Changing background color using radio button
+    var myBackground = remember {
+        mutableStateOf(Color.White)
+    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(myBackground.value),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+
+        Image(
+            painter = painterResource(myImage.value),
+            contentDescription = "myimage",
+            modifier = Modifier.size(300.dp)
+        )
+
+
         //Button
-        Button(onClick = {
-            myBackgroundcolor.value = Color.Black
-            myButtonText.value = "Compose is fun"
-        },
+        Button(
+            onClick = {
+                myBackgroundcolor.value = Color.Black
+                myButtonText.value = "Compose is fun"
+
+                //image
+                if (buttonStatus.value) {
+                    myImage.value = R.drawable.second
+                } else {
+                    myImage.value = R.drawable.first
+                }
+                //End of image
+            },
             colors = ButtonDefaults.buttonColors(myBackgroundcolor.value),
-            modifier = Modifier.size(250.dp,50.dp),
+            modifier = Modifier.size(250.dp, 50.dp),
             shape = RoundedCornerShape(10.dp),
             border = BorderStroke(2.dp, Color.Black)
         ) {
@@ -96,8 +146,10 @@ fun Home() {
         //TextField
         TextField(
             value = name,
-            onValueChange = { name = it},
-            modifier = Modifier.fillMaxWidth().padding(start = 10.dp,end=10.dp),
+            onValueChange = { name = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp),
             label = { Text(text = "Enter your name ") },
             colors = TextFieldDefaults.textFieldColors(
                 focusedLabelColor = Color.Gray,
@@ -105,12 +157,109 @@ fun Home() {
                 focusedIndicatorColor = Color.Yellow,
                 unfocusedIndicatorColor = Color.Red,
                 cursorColor = Color.Red,
-                
-            )
+
+                )
         )
 
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Dynamic Text and checkbox Component
+
+        Text(
+            text = myText.value,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .width(300.dp)
+                .background(Color.Red)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+
+        Column {
+
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Checkbox(
+                    checked=firstCheckbox.value,
+                    onCheckedChange = {
+                        firstCheckbox.value=it
+                        if (firstCheckbox.value) {
+                            myText.value = "Your gender is male"
+                        }
+                        else {
+                            myText.value = "What is your gender?"
+                        }
+
+                        },
+                    colors = CheckboxDefaults.colors(Color.Gray)
+                )
+                Text(
+                    text = "Male", fontSize = 20.sp
+                )
+            }
 
 
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Checkbox(
+                    checked=secondCheckbox.value,
+                    onCheckedChange = {
+                        secondCheckbox.value=it
+                        if (secondCheckbox.value) {
+                            myText.value = "Your gender is female"
+                        }
+                        else {
+                            myText.value = "What is your gender?"
+                        }
+                                      },
+                    colors = CheckboxDefaults.colors(Color.Gray)
+
+                )
+                Text(
+                    text = "Female", fontSize = 20.sp
+                )
+            }
+        }
+
+
+        //RadioButton
+
+
+        var radioTexts = listOf("Red","Green","Yellow")
+        var colorLIst = arrayListOf(Color.Red, Color.Green, Color.Yellow)
+        var radioIndex = remember { mutableStateOf(0) }
+
+        Text(
+            text = "Change screen background color....",
+            fontSize = 20.sp
+        )
+
+        Column {
+            radioTexts.forEachIndexed { position, name ->
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.clickable {
+                        radioIndex.value = position
+                    }){
+                    RadioButton(
+                        selected =
+                        name == radioTexts[radioIndex.value],
+                        onClick = {
+                            radioIndex.value = position
+                            myBackground.value = colorLIst[radioIndex.value]
+                        },
+                        colors = RadioButtonDefaults.colors(Color.Red)
+                    )
+                    Text(text = name, fontSize = 20.sp)
+                }
+
+            }
+        }
+
+        Button(onClick = { myBackground.value = colorLIst[radioIndex.value] },
+            colors = ButtonDefaults.buttonColors(Color.Gray),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text(text = "Change background color")
+        }
 
 
 
