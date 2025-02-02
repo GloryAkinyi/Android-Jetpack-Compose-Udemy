f8601dda1e685f7dca06a72c97c25136fb9fbea3
gloryakinyi <akinyiglory2@gmail.com>
2025-02-02 06:01:47 +0300

    initial commit

--- app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
@@ -8,17 +8,25 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,6 +46,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
 
@@ -46,6 +55,8 @@ fun Home() {
     val myBackgroundcolor = remember {
         mutableStateOf(Color.Red)
     }
+
+    var name by remember { mutableStateOf("") }
 
 
     val myButtonText = remember{
@@ -78,6 +89,30 @@ fun Home() {
                 textAlign = TextAlign.Center
             )
         }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        //TextField
+        TextField(
+            value = name,
+            onValueChange = { name = it},
+            modifier = Modifier.fillMaxWidth().padding(start = 10.dp,end=10.dp),
+            label = { Text(text = "Enter your name ") },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedLabelColor = Color.Gray,
+                unfocusedLabelColor = Color.Red,
+                focusedIndicatorColor = Color.Yellow,
+                unfocusedIndicatorColor = Color.Red,
+                cursorColor = Color.Red,
+                
+            )
+        )
+
+
+
+
+
 
 
     }
